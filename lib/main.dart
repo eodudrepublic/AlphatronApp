2515c814aa2e9b548bb5c5cbf7fc354726eb1b4d
Daeyoung Kim <eodudrepublic@gmail.com>
2024-08-20 06:12:34 +0900

    feat - WebRTC를 사용한 h264 영상 스트리밍 페이지 초기안 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:alphatron_app/view/google_sign_in/sign_in_page.dart';
+import 'package:alphatron_app/view/google_sign_in/user_info_page.dart';
+import 'package:alphatron_app/view/video_streaming/webrtc_video_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,12 +14,21 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Google Sign In Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SignInPage(),
+      initialRoute: '/root',
+      getPages: [
+        GetPage(name: '/root', page: () => WebRTCVideoView()),
+
+        GetPage(name: '/google_sign_in_view', page: () => SignInPage()),
+        GetPage(name: '/user_info_page', page: () => UserInfoPage()),
+
+        GetPage(name: '/webrtc_video_streaming', page: () => WebRTCVideoView()),
+      ],
+      home: WebRTCVideoView(),
     );
   }
 }
