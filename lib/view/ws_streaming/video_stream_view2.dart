610e7d514ca7efdd14c0a72dfacb24806bc33ed7
jhy0285 <jhy0285@gmail.com>
2024-09-21 22:45:48 +0900

    feature: mjpeg 구현

--- lib/view/ws_streaming/video_stream_view2.dart
+++ lib/view/ws_streaming/video_stream_view2.dart
@@ -1,9 +1,7 @@
-
 import 'dart:typed_data';
 
-import 'package:alphatron_app/model/websocket/websocket.dart';
 import 'package:flutter/material.dart';
-
+import 'package:web_socket_channel/io.dart';
 
 class VideoStream extends StatefulWidget {
   const VideoStream({Key? key}) : super(key: key);
@@ -13,17 +11,18 @@ class VideoStream extends StatefulWidget {
 }
 
 class _VideoStreamState extends State<VideoStream> {
-  final WebSocket _socket = WebSocket("ws://localhost:7777/test");
+  final IOWebSocketChannel _channel = IOWebSocketChannel.connect("ws://localhost:7777/test");
   bool _isConnected = false;
-  void connect(BuildContext context) async {
-    _socket.connect();
+  Uint8List? _currentFrame;
+
+  void connect(BuildContext context) {
     setState(() {
       _isConnected = true;
     });
   }
 
   void disconnect() {
-    _socket.disconnect();
+    _channel.sink.close();
     setState(() {
       _isConnected = false;
     });
@@ -45,42 +44,41 @@ class _VideoStreamState extends State<VideoStream> {
                 children: [
                   ElevatedButton(
                     onPressed: () => connect(context),
-
                     child: const Text("Connect"),
                   ),
                   ElevatedButton(
                     onPressed: disconnect,
-
                     child: const Text("Disconnect"),
                   ),
                 ],
               ),
-              const SizedBox(
-                height: 50.0,
-              ),
+              const SizedBox(height: 50.0),
               _isConnected
                   ? StreamBuilder(
-                stream: _socket.stream,
+                stream: _channel.stream,
                 builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return const Center(
+                      child: Text("Connection Closed!"),
+                    );
+                  }
+
                   if (!snapshot.hasData) {
                     return const CircularProgressIndicator();
                   }
 
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return const Center(
-                      child: Text("Connection Closed !"),
+                  // MJPEG 스트림에서 프레임을 추출하여 렌더링
+                  _currentFrame = extractJpegFrame(snapshot.data as Uint8List);
+
+                  if (_currentFrame != null) {
+                    return Image.memory(
+                      _currentFrame!,
+                      gaplessPlayback: true,
+                      excludeFromSemantics: true,
                     );
+                  } else {
+                    return const CircularProgressIndicator();
                   }
-                  //? Working for single frames
-                  return Image.memory(
-                    Uint8List.fromList(
-                      base64Decode(
-                        (snapshot.data.toString()),
-                      ),
-                    ),
-                    gaplessPlayback: true,
-                    excludeFromSemantics: true,
-                  );
                 },
               )
                   : const Text("Initiate Connection")
@@ -89,5 +87,39 @@ class _VideoStreamState extends State<VideoStream> {
         ),
       ),
     );
+  }
+
+  /// MJPEG 스트림에서 개별 JPEG 이미지를 추출하는 함수
+  Uint8List? extractJpegFrame(Uint8List data) {
+    // JPEG 이미지의 시작과 끝을 나타내는 바이트 시퀀스
+    final int soi = 0xFFD8; // Start of Image
+    final int eoi = 0xFFD9; // End of Image
+
+    int startIndex = -1;
+    int endIndex = -1;
+
+    // MJPEG 스트림에서 JPEG 이미지의 시작과 끝을 찾기
+    for (int i = 0; i < data.length - 1; i++) {
+      if (data[i] == 0xFF && data[i + 1] == 0xD8) {
+        startIndex = i;
+      }
+      if (data[i] == 0xFF && data[i + 1] == 0xD9) {
+        endIndex = i + 1;
+        break;
+      }
+    }
+
+    // JPEG 이미지가 유효한 경우 해당 부분을 추출
+    if (startIndex != -1 && endIndex != -1) {
+      return data.sublist(startIndex, endIndex + 1);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
   }
 }
