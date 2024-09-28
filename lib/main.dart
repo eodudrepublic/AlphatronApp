7a41541c0dc1d1e27426e13344800c6dd1a6cd16
jhy0285 <jhy0285@gmail.com>
2024-09-28 19:44:30 +0900

    구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:alphatron_app/view/google_sign_in/sign_in_page.dart';
 import 'package:alphatron_app/view/google_sign_in/user_info_page.dart';
 import 'package:alphatron_app/view/rtc_streaming/webrtc_video_view.dart';
+import 'package:alphatron_app/view/ws_streaming/mp4_stream_view.dart';
 import 'package:alphatron_app/view/ws_streaming/video_stream_view.dart';
 import 'package:alphatron_app/view/ws_streaming/video_stream_view2.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
-
+import 'dart:developer' as developer;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -23,14 +25,14 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/root',
       getPages: [
-        GetPage(name: '/root', page: () => VideoStreamView2()),
+        GetPage(name: '/root', page: () => VideoStreamingScreen()),
         GetPage(name: '/google_sign_in_view', page: () => SignInPage()),
         GetPage(name: '/user_info_page', page: () => UserInfoPage()),
         GetPage(
             name: '/websocket_video_streaming', page: () => WebRTCVideoView()),
         GetPage(name: '/webrtc_video_streaming', page: () => WebRTCVideoView()),
       ],
-      home: VideoStreamView2(),
+      home: VideoStreamingScreen(),
     );
   }
 }
