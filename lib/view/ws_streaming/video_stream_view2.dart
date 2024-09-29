341e9a39a0383fda5a7c2634b48c2de4df5673f1
jhy0285 <jhy0285@gmail.com>
2024-09-29 23:10:59 +0900

    feature : 실시간 전송 완료

--- lib/view/ws_streaming/video_stream_view2.dart
+++ lib/view/ws_streaming/video_stream_view2.dart
@@ -1,31 +1,62 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
+import '../../model/websocket/websocket.dart';
 
-class VideoStreamView2 extends StatefulWidget {
-  const VideoStreamView2({Key? key}) : super(key: key);
+class VideoStream extends StatefulWidget {
+  const VideoStream({Key? key}) : super(key: key);
 
   @override
-  State<VideoStreamView2> createState() => _VideoStreamState();
+  State<VideoStream> createState() => _VideoStreamState();
 }
 
-class _VideoStreamState extends State<VideoStreamView2> {
-  final IOWebSocketChannel _channel = IOWebSocketChannel.connect("ws://localhost:7777/test");
+class _VideoStreamState extends State<VideoStream> {
+  final WebSocket _socket = WebSocket("ws://192.168.0.23:7777/test");
   bool _isConnected = false;
-  Uint8List? _currentFrame;
+  Uint8List? _currentFrame; // 현재 프레임을 저장할 변수
 
-  void connect(BuildContext context) {
+  void connect(BuildContext context) async {
+    _socket.connect();
+    _socket.stream.listen((data) {
+      // 이전 프레임 처리 후 일정 시간 대기
+      Future.delayed(Duration(milliseconds: 33), () {
+        setState(() {
+          List<Uint8List> frames = _extractFrames(data as Uint8List);
+          if (frames.isNotEmpty) {
+            _currentFrame = frames.last;
+          }
+        });
+      });
+    });
     setState(() {
       _isConnected = true;
     });
   }
 
   void disconnect() {
-    _channel.sink.close();
+    _socket.disconnect();
     setState(() {
       _isConnected = false;
+      _currentFrame = null;
     });
+  }
+
+  List<Uint8List> _extractFrames(Uint8List data) {
+    List<Uint8List> frames = [];
+    int offset = 0;
+
+    while (offset < data.length) {
+      if (offset + 4 > data.length) break;
+
+      int frameSize = ByteData.sublistView(data, offset, offset + 4).getInt32(0, Endian.big);
+      offset += 4;
+
+      if (offset + frameSize > data.length) break;
+
+      frames.add(data.sublist(offset, offset + frameSize));
+      offset += frameSize;
+    }
+
+    return frames;
   }
 
   @override
@@ -43,7 +74,9 @@ class _VideoStreamState extends State<VideoStreamView2> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ElevatedButton(
-                    onPressed: () => connect(context),
+                    onPressed: () {
+                      connect(context);
+                    },
                     child: const Text("Connect"),
                   ),
                   ElevatedButton(
@@ -52,74 +85,22 @@ class _VideoStreamState extends State<VideoStreamView2> {
                   ),
                 ],
               ),
-              const SizedBox(height: 50.0),
+              const SizedBox(
+                height: 50.0,
+              ),
               _isConnected
-                  ? StreamBuilder(
-                stream: _channel.stream,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return const Center(
-                      child: Text("Connection Closed!"),
-                    );
-                  }
-
-                  if (!snapshot.hasData) {
-                    return const CircularProgressIndicator();
-                  }
-
-                  // MJPEG 스트림에서 프레임을 추출하여 렌더링
-                  _currentFrame = extractJpegFrame(snapshot.data as Uint8List);
-
-                  if (_currentFrame != null) {
-                    return Image.memory(
-                      _currentFrame!,
-                      gaplessPlayback: true,
-                      excludeFromSemantics: true,
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                },
+                  ? (_currentFrame != null
+                  ? Image.memory(
+                _currentFrame!,
+                gaplessPlayback: true,
+                excludeFromSemantics: true,
               )
-                  : const Text("Initiate Connection")
+                  : const CircularProgressIndicator())
+                  : const Text("Initiate Connection"),
             ],
           ),
         ),
       ),
     );
-  }
-
-  /// MJPEG 스트림에서 개별 JPEG 이미지를 추출하는 함수
-  Uint8List? extractJpegFrame(Uint8List data) {
-    // JPEG 이미지의 시작과 끝을 나타내는 바이트 시퀀스
-    final int soi = 0xFFD8; // Start of Image
-    final int eoi = 0xFFD9; // End of Image
-
-    int startIndex = -1;
-    int endIndex = -1;
-
-    // MJPEG 스트림에서 JPEG 이미지의 시작과 끝을 찾기
-    for (int i = 0; i < data.length - 1; i++) {
-      if (data[i] == 0xFF && data[i + 1] == 0xD8) {
-        startIndex = i;
-      }
-      if (data[i] == 0xFF && data[i + 1] == 0xD9) {
-        endIndex = i + 1;
-        break;
-      }
-    }
-
-    // JPEG 이미지가 유효한 경우 해당 부분을 추출
-    if (startIndex != -1 && endIndex != -1) {
-      return data.sublist(startIndex, endIndex + 1);
-    } else {
-      return null;
-    }
-  }
-
-  @override
-  void dispose() {
-    _channel.sink.close();
-    super.dispose();
   }
 }
